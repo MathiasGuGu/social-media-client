d4a3d0a738aceb4fb9bfe61a5ae59e4fc09f47ac
mathiasgugu <magugu@Mathiass-MacBook-Pro.local>
2023-11-14 16:18:45 +0100

    tests

---
+++ src/js/api/auth/login.test.js
@@ -0,0 +1,30 @@
+import { login } from "./login";
+
+const storageFix = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn(),
+};
+global.localStorage = storageFix;
+
+describe("login", () => {
+  it("Should retrieve login token", async () => {
+    const userName = "matgum51873@stud.noroff.no";
+    const password = "aaiiuues";
+
+    const data = await login(userName, password);
+
+    expect(typeof data).toBe("object");
+  });
+
+  it("place token in local storage", async () => {
+    localStorage.setItem("token", "dummyToken");
+    const userName = "tulling@stud.noroff.no";
+    const password = "tulling1234";
+
+    await login(userName, password);
+
+    const accessToken = localStorage.getItem("token");
+    expect(accessToken).not.toBeNull();
+  });
+});
